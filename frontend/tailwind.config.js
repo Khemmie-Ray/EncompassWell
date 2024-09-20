c5ff8840cc532b03058c903749239dba431edd18
Khemmie-Ray <atokemmy@gmail.com>
2024-09-20 12:14:28 +0100

    setting up codebase

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#030A04',
+        'secondary': '#B9A7C3',
+        'grey': '#6C6C6C',
+        'lightGrey': '#B9A5C4'
+      },
+      fontFamily: {
+        'Geist': ['Geist Sans', 'sans-serif'],
+        'InstrumentSerif': ["Instrument Serif", "serif"]
+      }
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
