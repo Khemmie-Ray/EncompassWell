519b83b51d9bfd5e3e4f5cb5a87b158174bf90ae
the-first-elder <emmanuelmayowa95@gmail.com>
2024-09-22 05:05:27 +0100

    new ca nft

--- smart contract/src/ECWNFT.sol
+++ smart contract/src/ECWNFT.sol
@@ -2,14 +2,15 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC721Enumerable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract ECWNFT is ERC721, ERC721URIStorage, ERC721Burnable {
+contract ECWNFT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable {
     uint256 private _nextTokenId;
 
-    constructor() ERC721("EncompassWell", "ECW") {}
+    constructor() ERC721("EncompassWell", "ECW"){}
 
     function safeMint(address to, string memory uri) public {
         uint256 tokenId = _nextTokenId++;
@@ -17,13 +18,50 @@ contract ECWNFT is ERC721, ERC721URIStorage, ERC721Burnable {
         _setTokenURI(tokenId, uri);
     }
 
-    // The following functions are overrides required by Solidity.
+    // Override necessary functions to resolve inheritance conflicts
 
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+    function getUserNFTs(address user) external view returns (uint256[] memory) {
+        uint256 balance = balanceOf(user); 
+        uint256[] memory tokenIds = new uint256[](balance);
+
+        // Retrieve all token IDs owned by the user
+        for (uint256 i = 0; i < balance; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(user, i);
+        }
+
+        return tokenIds;
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
+
+
+
+    function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable) {
+      super._increaseBalance(account, value);
+    }
+
+     function _update(
+      address to,
+      uint256 tokenId,
+      address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+      return super._update(to, tokenId, auth);
+    }
 }
+//  new ca 0x8eC79AFcf77D3d211863B95eEf08679A5Bfc8577
---
+++ smart contract/test/nftTest.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/ECWNFT.sol";  // Adjust the path based on your project structure
+
+contract ECWNFTTest is Test {
+    ECWNFT private nftContract;
+
+    address private owner = address(0x123);
+    address private user1 = address(0x456);
+    address private user2 = address(0x789);
+
+    function setUp() public {
+        // Deploy the ECWNFT contract
+        nftContract = new ECWNFT();
+    }
+
+    function testMintAndTokenOfOwnerByIndex() public {
+        // Mint tokens to different users
+        nftContract.safeMint(owner, "tokenURI1");
+        nftContract.safeMint(user1, "tokenURI2");
+        nftContract.safeMint(user2, "tokenURI3");
+        nftContract.safeMint(user1, "tokenURI4"); // Minting multiple to the same user
+
+        // Verify token balances
+        assertEq(nftContract.balanceOf(owner), 1);
+        assertEq(nftContract.balanceOf(user1), 2);
+        assertEq(nftContract.balanceOf(user2), 1);
+
+        // Check `tokenOfOwnerByIndex`
+        uint256 ownerTokenId = nftContract.tokenOfOwnerByIndex(owner, 0);
+        assertEq(ownerTokenId, 0);  // First minted token should be tokenId 0
+
+        uint256 user1TokenId1 = nftContract.tokenOfOwnerByIndex(user1, 0);
+        uint256 user1TokenId2 = nftContract.tokenOfOwnerByIndex(user1, 1);
+        assertEq(user1TokenId1, 1);  // First minted token to user1
+        assertEq(user1TokenId2, 3);  // Second minted token to user1
+
+        uint256 user2TokenId = nftContract.tokenOfOwnerByIndex(user2, 0);
+        assertEq(user2TokenId, 2);  // First minted token to user2
+    }
+
+     function testGetAllNFTsOwnedByUser() public {
+        // Mint tokens to different users
+        nftContract.safeMint(owner, "tokenURI1");
+        nftContract.safeMint(user1, "tokenURI2");
+        nftContract.safeMint(user2, "tokenURI3");
+        nftContract.safeMint(user1, "tokenURI4"); // Minting multiple to the same user
+
+        uint256 balanceUser1 = nftContract.balanceOf(user1);
+        uint256[] memory user1TokenIds = new uint256[](balanceUser1);
+
+        // Retrieve all token IDs owned by user1
+        for (uint256 i = 0; i < balanceUser1; i++) {
+            user1TokenIds[i] = nftContract.tokenOfOwnerByIndex(user1, i);
+        }
+
+        // Assert the token IDs are as expected
+        assertEq(user1TokenIds[0], 1);  // First token ID owned by user1
+        assertEq(user1TokenIds[1], 3);  // Second token ID owned by user1
+    }
+}
