e281e36e5b4a8b7c7f194ba8adcaac92c0b63761
Khemmie-Ray <atokemmy@gmail.com>
2024-10-17 14:30:19 +0100

    vercel minor fixes

--- frontend/src/pages/Dashboard/AIChat.jsx
+++ frontend/src/pages/Dashboard/AIChat.jsx
@@ -11,7 +11,6 @@ import {
 } from "../../helpers/helper";
 import ConnectButton from "../../components/ConnectButton";
 import MintNft from "../../components/MintNft";
-import playicon from "../../assets/playIcon.svg";
 import Frequency from "../../components/loaders/Frequency";
 
 const AIChat = () => {
